9f70becd6b34d01ca744b33c58550fae667d3623
Thadeus <ainojie@gmail.com>
2017-10-13 09:23:23 +0100

    Grant url access to all clients browsers and define allowed vers

--- app.js
+++ app.js
@@ -25,6 +25,16 @@ db.once("open", () => {
     //All database communication goes here
 });
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use("/resources",routes);
 
 // catch 404 and forward to error handler
--- routes.js
+++ routes.js
@@ -40,6 +40,7 @@ router.put("/:id", (req, res, next) => {
         if (doc == null) return next (err);
         doc.update(req.body, (err) => {
             if (err) return next (err);
+            res.status(201);
             res.json({
                 response: "You sent to UPDATE " + doc.name
             })
@@ -55,7 +56,7 @@ router.delete("/:id", (req, res, next) => {
         if (doc == null) return next(err);
         Student.remove({_id: req.params.id}, (err) => {
             if (err) return next(err);
-            res.status = 201;
+            res.status(201);
             res.json({
                 response: "You have sent to delete the user :" + doc.name
             })
