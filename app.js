af2523ee7aaaaed8cc09a52e82e470cbc07d51f4
Thadeus Ajayi <Thadeus Ajayi>
2017-11-04 18:46:46 +0100

    correct allow origin in second line to allow headers in access control

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ db.once("open", () => {
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if (req.method === "OPTIONS") {
         res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
         return res.status(200).json({});
